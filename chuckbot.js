cf38e82b159b954ea7116c3ffd5408f5e8bdf0b2
Per Jansson <per.r.jansson@gmail.com>
2015-12-22 20:48:16 +0100

    avoid infinite loop

--- chuckbot.js
+++ chuckbot.js
@@ -15,6 +15,10 @@ module.exports = function(req, res, next) {
       text: joke
     };
 
-    return res.status(200).json(botPayload);
+    if (userName !== 'slackbot') {
+      return res.status(200).json(botPayload);
+    } else {
+      return res.status(200).end();
+    }
   })
 }
